af081326dd9dc0039b4f744d9fd98187490e1a55
Nils J. Haugen <nils+git@gaupne.net>
2023-09-30 14:50:45 +0200

    chore: move to GH actions for publising

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nilsjha.github.io",
-  outDir: "docs",
-  base: "/",
   integrations: [tailwind()],
 });
