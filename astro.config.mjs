11efe9cfb3a274872a42fc5796d6a8bca7cf6de4
Nils Jarle Haugen <nilsjha+commit@gmail.com>
2023-10-07 20:03:57 +0200

    feat: add solidjs dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nilsjha.github.io",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    solidJs({
+      include: ["**/solid/*"],
+    }),
+  ],
 });
